197f09f1cae6107f14858a7c0d8ee027ea2fc60c
Jonas Sandberg <jonas.sandb@gmail.com>
2021-02-24 11:22:52 +0100

    Use ts in client entrypoints
    This does not work however, timestamps are still grouped.
    Also clean the structure of the code, fix the stats script, mount entrypoints directly

--- src/dynamic.sh
+++ src/dynamic.sh
@@ -6,18 +6,26 @@ echo "dc-server, 11211" > "/usr/src/memcached/memcached_client/servers.txt"
 benchmark_step() {
     local rps=$1
 
-    printf "\n\nRPS: $rps\nTimeout: $STEP_TIME\n =======================\n"
+    echo "\n"
+    echo "RPS: $rps"
+    echo "Timeout: $STEP_TIME"
+    echo "======================="
+
+    for i in $(seq $REPORTS_PER_STEP); do
+        echo "DYNAMIC RPS: $rps"
+    done
+
     /usr/src/memcached/memcached_client/loader \
         -a /usr/src/memcached/twitter_dataset/twitter_dataset_30x \
         -s /usr/src/memcached/memcached_client/servers.txt \
-        -c $CONNECTIONS -w $CLIENT_WORKERS -e -r $rps -t $STEP_TIME -T $STATS_INTERVAL
-    }
+        -c $CONNECTIONS -w $CLIENT_WORKERS -e -r $rps -t $STEP_TIME -T $STEP_INTERVAL
+}
 
 if [[ -n $1 ]]; then
     RPS=$1
 
     STEP_TIME=$(($RUNTIME / 5))
-    STATS_INTERVAL=$(($STATS_INTERVAL / 5))
+    REPORTS_PER_STEP=$(($STEP_TIME / $STEP_INTERVAL))
 
     RPS_LOW=$(($RPS / 4)) # 25 %
     RPS_MID=$(($RPS / 2)) # 50 %
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -ex
-#set -x
 
 echo "dc-server, 11211" > "/usr/src/memcached/memcached_client/servers.txt"
 
 if [[ -n $1 ]]; then
     RPS=$1
-
-    # test -f /usr/src/memcached/twitter_dataset/twitter_dataset_30x || (echo "Scaled dataset not available"; exit)
 
     printf "\n\nRPS: $RPS\nTimeout: $RUNTIME\n =======================\n"
 
@@ -15,7 +12,8 @@ if [[ -n $1 ]]; then
     /usr/src/memcached/memcached_client/loader \
         -a /usr/src/memcached/twitter_dataset/twitter_dataset_30x \
         -s /usr/src/memcached/memcached_client/servers.txt \
-        -c $CONNECTIONS -w $CLIENT_WORKERS -e -r $RPS -t $RUNTIME -T $STATS_INTERVAL
+        -c $CONNECTIONS -w $CLIENT_WORKERS -e -r $RPS -t $RUNTIME -T $STATS_INTERVAL \
+        | ts "%F %R:%.S%z"
 else
     bash
 fi
