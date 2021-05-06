cb7636c56a527a216278e923d6e47abd0f177f92
Jonas Sandberg <jonas.sandb@gmail.com>
2021-05-06 02:34:56 +0200

    Log eris cache allocations, use combined stress-ng profile

--- dc-client/entrypoints/warmup.sh
+++ dc-client/entrypoints/warmup.sh
@@ -9,7 +9,7 @@ cd /usr/src/memcached/memcached_client/
 ./loader \
 	-a ../twitter_dataset/twitter_dataset_30x \
 	-s servers.txt \
-    -c ${CONNECTIONS:=450} -w ${CLIENT_WORKERS:=18} -e -D ${SERVER_MEMORY:=65442} -j -T 1000 \
+    -c ${CONNECTIONS:=450} -w ${CLIENT_WORKERS:=18} -e -D ${SERVER_MEMORY:=65442} -j -S 1 \
 
 echo "Warmup completed"
 
